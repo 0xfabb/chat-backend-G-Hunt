4e48fb85987f74a7b5bcd2e09ece3bc97257c120
Vansh Khanna <vanshdev2305@gmail.com>
2025-05-20 17:52:48 +0530

    Refactored backend

--- src/index.ts
+++ src/index.ts
@@ -17,30 +17,58 @@ class Person {
 const roomArray: Person[] = [];
 
 wss.on("connection", (ws) => {
+  let currentPerson: Person | null = null;
+
   ws.on("message", (data: string) => {
     try {
       const message = JSON.parse(data);
       const roomId = message.roomId;
       const clientName = message.Name;
       const websocketConn = ws;
+
       if (message.type === "intro") {
+        // Remove any existing user with same socket to avoid duplicates
+        const filtered = roomArray.filter(p => p.websocketConn !== ws);
+        roomArray.length = 0;
+        roomArray.push(...filtered);
+
         const newPerson = new Person(roomId, clientName, websocketConn);
         roomArray.push(newPerson);
+        currentPerson = newPerson;
       } else {
+        // Broadcast the message to everyone else in the same room
         roomArray.forEach((person) => {
-  if (person.roomId === roomId && person.websocketConn !== websocketConn) {
-    person.websocketConn.send(
-      JSON.stringify({
-        Name: clientName,
-        data: message.data,
-        roomId: roomId,
-      })
-    );
-  }
+          if (
+            person.roomId === roomId &&
+            person.websocketConn !== websocketConn
+          ) {
+            person.websocketConn.send(
+              JSON.stringify({
+                Name: clientName,
+                data: message.data,
+                roomId: roomId,
+              })
+            );
+          }
         });
       }
     } catch (error) {
       console.log(error, "occured");
     }
+  });
+
+  ws.on("close", () => {
+    if (currentPerson) {
+      const index = roomArray.findIndex(
+        (p) => p.websocketConn === currentPerson?.websocketConn
+      );
+      if (index !== -1) {
+        roomArray.splice(index, 1);
+      }
+    }
+  });
+
+  ws.on("error", (err) => {
+    console.log("WebSocket error:", err);
   });
 });
