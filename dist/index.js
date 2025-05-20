4770c69e531fda452a79565e1470c041ba7803c2
0xfabb <vanshdev2305@gmail.com>
2025-05-20 17:54:35 +0530

    changed javascript

--- dist/index.js
+++ dist/index.js
@@ -11,6 +11,7 @@ class Person {
 }
 const roomArray = [];
 wss.on("connection", (ws) => {
+    let currentPerson = null;
     ws.on("message", (data) => {
         try {
             const message = JSON.parse(data);
@@ -18,14 +19,24 @@ wss.on("connection", (ws) => {
             const clientName = message.Name;
             const websocketConn = ws;
             if (message.type === "intro") {
+                // Remove any existing user with same socket to avoid duplicates
+                const filtered = roomArray.filter(p => p.websocketConn !== ws);
+                roomArray.length = 0;
+                roomArray.push(...filtered);
                 const newPerson = new Person(roomId, clientName, websocketConn);
                 roomArray.push(newPerson);
+                currentPerson = newPerson;
             }
             else {
+                // Broadcast the message to everyone else in the same room
                 roomArray.forEach((person) => {
                     if (person.roomId === roomId &&
-                        person.websocketConn != websocketConn) {
-                        person.websocketConn.send(JSON.stringify(message.data));
+                        person.websocketConn !== websocketConn) {
+                        person.websocketConn.send(JSON.stringify({
+                            Name: clientName,
+                            data: message.data,
+                            roomId: roomId,
+                        }));
                     }
                 });
             }
@@ -33,5 +44,16 @@ wss.on("connection", (ws) => {
         catch (error) {
             console.log(error, "occured");
         }
+    });
+    ws.on("close", () => {
+        if (currentPerson) {
+            const index = roomArray.findIndex((p) => p.websocketConn === (currentPerson === null || currentPerson === void 0 ? void 0 : currentPerson.websocketConn));
+            if (index !== -1) {
+                roomArray.splice(index, 1);
+            }
+        }
+    });
+    ws.on("error", (err) => {
+        console.log("WebSocket error:", err);
     });
 });
